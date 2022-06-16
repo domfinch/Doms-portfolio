562fef0518f3d1d11bede423909bca00911fbef7
domfinch <finch.163@buckeyemail.osu.edu>
2022-06-16 17:58:36 -0400

    first commit

---
+++ src/component/Header/header.js
@@ -0,0 +1,12 @@
+import Nav from "../Nav/nav";
+
+function Header() {
+    return (
+      <div className="Header">
+          <h1>Dominic</h1>
+        <Nav />
+      </div>
+    );
+  }
+
+  export default Header;
---
+++ src/component/Nav/nav.js
@@ -0,0 +1,19 @@
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">About Me</a>
+        </li>
+        <li>
+          <a href="#">Work</a>
+        </li>
+        <li>
+          <a href="#">Contact Information</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
